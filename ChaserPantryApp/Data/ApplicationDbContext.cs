8540fa90a22362441bc554489c9aa93fa265f6d7
Heinrich Gottberg <heinrichg16@yahoo.com>
2026-04-02 13:04:04 -0400

    Added Project to Repo

---
+++ ChaserPantryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChaserPantryApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
