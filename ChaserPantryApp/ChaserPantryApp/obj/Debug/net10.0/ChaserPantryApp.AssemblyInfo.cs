6c4416a45d8b1ac0a9208959c70f8c57a959add6
Shawn Conboy <conboysbusiness@gmail.com>
2026-04-20 23:16:17 -0400

    Updated home page ui

--- ChaserPantryApp/ChaserPantryApp/obj/Debug/net10.0/ChaserPantryApp.AssemblyInfo.cs
+++ ChaserPantryApp/ChaserPantryApp/obj/Debug/net10.0/ChaserPantryApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChaserPantryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f4fed3b944b3e87f41f507cc5d11f01f3b3624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c45522095dcfb008963b72be5d6789afaec234e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChaserPantryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChaserPantryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
