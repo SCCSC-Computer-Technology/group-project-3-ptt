c28c9d2ba412d0a61d577f39c15caaba46777307
Rayraygetwild <rae5086@gmail.com>
2026-04-22 14:09:51 -0400

    Privacy page update , multiple pages added
    I updated the Privacy page
    
    I also added a couple of pages so that when certain cards are clicked, you can go to those pages.
    
    And I made it so the users can't click on any of the cards that allow you to go into more information unless they're logged in and instead if they click it will leave them to the login page

---
+++ PantryApp/Pantreats/Areas/Identity/Pages/Account/ApplyOnline.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pantreats.Areas.Identity.Pages.Account
+{
+    public class ApplyOnlineModel : PageModel
+    {
+        //public class LoginModel : PageModel
+    }
+}
---
+++ PantryApp/Pantreats/Areas/Identity/Pages/Account/Manage/Shop.cshtml.cs
@@ -0,0 +1,6 @@
+﻿namespace Pantreats.Areas.Identity.Pages.Account.Manage
+{
+    public class Shop
+    {
+    }
+}
