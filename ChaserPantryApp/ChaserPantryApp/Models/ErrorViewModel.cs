411c934ff04b397f43fdd540bd01226f1fea46f6
Shawn Conboy <conboysbusiness@gmail.com>
2026-04-22 05:00:30 -0400

    Restructure project

--- ChaserPantryApp/ChaserPantryApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChaserPantryApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ PantryApp/Pantreats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pantreats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
