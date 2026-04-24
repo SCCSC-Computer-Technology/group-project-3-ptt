63c4ce419c2501b2c828aa629808b567015fe093
Shawn Conboy <conboysbusiness@gmail.com>
2026-04-24 15:05:25 -0400

    update vendors tab. Add crud and related pages.

--- PantryApp/Pantreats/Data/ApplicationDbContext.cs
+++ PantryApp/Pantreats/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Pantreats.Models;
 
 namespace Pantreats.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Vendor> Vendors { get; set; } = default!;
     }
 }
