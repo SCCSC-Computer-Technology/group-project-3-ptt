7d099db92282dc6766806ac33812e0af6099dbea
Shawn Conboy <conboysbusiness@gmail.com>
2026-04-24 05:28:22 -0400

    Updated student section

---
+++ PantryApp/Pantreats/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pantreats.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.StudentId = id;
+            return View();
+        }
+    }
+}
